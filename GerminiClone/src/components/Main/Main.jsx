a9ab7fdc3892237e3ffa57a816fc2a112f70aec6
Dikgobe <djmolepo@gmail.com>
2025-10-01 12:37:17 +0200

    Added new features and styling to the main.jsx file

--- GerminiClone/src/components/Main/Main.jsx
+++ GerminiClone/src/components/Main/Main.jsx
@@ -9,6 +9,28 @@ const main = () => {
             <p>DjGerminiClone</p>
             <img src={assets.user_icon} alt="" />
         </div>
+        <div className="main-container">
+            <p><span>Hello, Recruiter</span></p>
+            <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <p>Suggest beautiful places to go on your next roadtrip</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Briefly summarize this concepts: React js</p>
+                <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Brainstorm team bonding activities for your work retreat</p>
+                <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Improve the readability of the following code</p>
+                <img src={assets.code_icon} alt="" />
+            </div>
+        </div>
     </div>
   )
 }
